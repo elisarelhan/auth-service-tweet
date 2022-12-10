774b894c27dcd8b798d937502ba6246de8f90f5a
elisarelhan <79890999+elisarelhan@users.noreply.github.com>
2022-12-11 03:46:11 +0530

    Update UserService.java

--- src/main/java/com/tweetapp/auth/service/UserService.java
+++ src/main/java/com/tweetapp/auth/service/UserService.java
@@ -15,23 +15,23 @@ import com.tweetapp.auth.repository.UserRepo;
 public class UserService {
 
 	@Autowired
-	private UserRepo userRepo;
-	private static final String USER_CREATED_TOPIC = "user";
-	@Autowired
-	private KafkaTemplate<String, User> kafkaTemplate;
-	@Autowired
-	private KafkaTemplate<String, List<User>> template;
-	@Autowired
-private KafkaTemplate<String, String> templateString;
+// 	private UserRepo userRepo;
+// 	private static final String USER_CREATED_TOPIC = "user";
+// 	@Autowired
+// 	private KafkaTemplate<String, User> kafkaTemplate;
+// 	@Autowired
+// 	private KafkaTemplate<String, List<User>> template;
+// 	@Autowired
+// private KafkaTemplate<String, String> templateString;
 	public List<User> findAllUsers() {
 		List<User> users = new ArrayList<>();
 		users = userRepo.findAll();
 
 		if (users.isEmpty()) {
-			templateString.send(USER_CREATED_TOPIC, "No User Found");
+// 			templateString.send(USER_CREATED_TOPIC, "No User Found");
 			throw new UserNotFoundException("No User Found");
 		} else {
-			template.send(USER_CREATED_TOPIC, users);
+// 			template.send(USER_CREATED_TOPIC, users);
 			return users;
 		}
 	}
@@ -40,17 +40,17 @@ private KafkaTemplate<String, String> templateString;
 		List<User> usersByUsername = new ArrayList<>();
 		usersByUsername = userRepo.findByUserName(username);
 		if (usersByUsername.isEmpty()) {
-			templateString.send(USER_CREATED_TOPIC, "User Not Found");
+// 			templateString.send(USER_CREATED_TOPIC, "User Not Found");
 			throw new UserNotFoundException("User Not Found");
 		} else {
-		template.send(USER_CREATED_TOPIC, usersByUsername);
+// 		template.send(USER_CREATED_TOPIC, usersByUsername);
 			return usersByUsername;
 		}
 	}
 
 	public User findUserByEmail(String email) {
 		User userDetails = userRepo.findByEmail(email);
-		kafkaTemplate.send(USER_CREATED_TOPIC, userDetails);
+// 		kafkaTemplate.send(USER_CREATED_TOPIC, userDetails);
 		return userDetails;
 
 	}
